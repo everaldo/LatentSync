08f8191b7a934f69746978183cc39cccf867a746
Everaldo Gomes <everaldo.gomes@gmail.com>
2025-11-13 23:19:49 -0300

    Update inference.sh
    Prepending with conda for Running on Google Colab

--- inference.sh
+++ inference.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python -m scripts.inference \
+conda run -n latentsync python -m scripts.inference \
     --unet_config_path "configs/unet/stage2_512.yaml" \
     --inference_ckpt_path "checkpoints/latentsync_unet.pt" \
     --inference_steps 20 \
